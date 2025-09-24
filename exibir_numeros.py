bf33a9ae71ae4904597ca1630c75f269be6d3658
Henrique8350 <ph345468@gmail.com>
2025-09-24 14:35:26 -0300

    Atualizando a Branch na pasta exibir_numeros e criação da atividade tabuada.py

---
+++ exibir_numeros.py
@@ -0,0 +1,14 @@
+"""
+for numero in range(1,11):
+    print("Número:",numero)
+"""
+
+soma = 0
+
+for i in range(1,10+1):
+    numero = int(input(f"Digite o {i}° número: "))
+    soma += numero
+
+print("A soma de todos os números é: ",soma)
+    
+    
---
+++ tabuada.py
@@ -0,0 +1,8 @@
+multiplicacao = 1
+numero = int(input("Digite um número: "))
+vezesnum = 10
+for i in range(11):
+    multiplicacao = numero*i
+    print(f"{numero} x {i} = {multiplicacao}")
+          
+    print("multiplicação de todos os valores:",i*vezesnum)
